12fc3e78e8eb6dd69fdb650c47f7a7d494ac00ac
glontianano <glonti1899@gmail.com>
2020-06-07 21:55:12 +0400

    Change async functions to promises.

--- app.js
+++ app.js
@@ -46,19 +46,21 @@ let users = {};
 
 io.on('connection', function (socket) {
     //Create Users array with all connected user.
-    socket.on('new-user', async function (data) {
+    socket.on('new-user', function (data) {
         let jwt = data.jwt;
         let decodedJwt = jwtDecode(jwt);
-        let id = decodedJwt.user.id;socket
-        let userInfo = await userModel.findById(id);
-        socket.username = userInfo.username;
-        // username should be equal to user socket
-        // to defined receiver in private messaging.
-        users[socket.username] = socket;
+        let id = decodedJwt.user.id;
+        userModel.findById(id).then(function (data) {
+            socket.username = data.username;
+            // username should be equal to user socket
+            // to defined receiver in private messaging.
+            users[socket.username] = socket;
+        });
+
     })
 
     // Send old messages to client.
-    socket.on('old-messages',  function (data) {
+    socket.on('old-messages', function (data) {
         let receiver = data.receiver;
         let jwt = data.sender;
         let decodedJwt = jwtDecode(jwt);
@@ -87,55 +89,54 @@ io.on('connection', function (socket) {
 
     // Send message in real time if user is online.
     // And save in database.
-    socket.on('send-message', async function (msg) {
+    socket.on('send-message', function (msg) {
         let message = msg.message;
         let receiver = msg.receiver;
         let jwt = msg.sender;
         let decodedJwt = jwtDecode(jwt);
         let id = decodedJwt.user.id;
-        let senderInfo = await userModel.findById(id);
-        let sender = senderInfo.username;
-        let roomName = sender.concat('', receiver);
-        let roomName2 = receiver.concat('', sender);
+        userModel.findById(id).then(function (user) {
+            let sender = user.username;
+            let roomName = sender.concat('', receiver);
+            let roomName2 = receiver.concat('', sender);
 
-        //roomName = ananofarna
-        //roomName2 = farnanano
-
-        //Check if roomName already exists.
-        await chatModel.find({roomName: roomName}, function (err, doc) {
-            if (err) throw err;
-            if (doc.length > 0) {
-                sendMessage(roomName);
-            }
-            else {
-                 chatModel.find({roomName: roomName2}, function (err, doc) {
-                    if (err) throw err;
-                     if (doc.length > 0) {
-                         sendMessage(roomName2);
-                    }
-                     // else create new room
-                     else {
-                         sendMessage(roomName);
-                     }
-                })
-            }
-        })
-
-        function sendMessage(roomName){
-            let newMessage = new chatModel({
-                roomName: roomName,
-                sender: sender,
-                message: message,
-                receiver: receiver,
-            });
-            newMessage.save(function (err) {
-                if (err) throw err;
-                //If receiver is connected (is online), send message.
-                if (receiver in users) {
-                    users[receiver].emit('new-message', {message: message, sender: sender});
+            //Check if roomName already exists.
+            chatModel.find({roomName: roomName}).then(function (doc) {
+                if (doc.length > 0) {
+                    sendMessage(roomName);
+                }
+                else {
+                    chatModel.find({roomName: roomName2}).then(function (doc) {
+                        if (doc.length > 0) {
+                            sendMessage(roomName2);
+                        }
+                        // else create new room
+                        else {
+                            sendMessage(roomName);
+                        }
+                    })
                 }
             })
-        }
+
+            function sendMessage(roomName){
+                let newMessage = new chatModel({
+                    roomName: roomName,
+                    sender: sender,
+                    message: message,
+                    receiver: receiver,
+                });
+                newMessage.save(function (err) {
+                    if (err) throw err;
+                    //If receiver is connected (is online), send message.
+                    if (receiver in users) {
+                        users[receiver].emit('new-message', {message: message, sender: sender});
+                    }
+                })
+            }
+
+        });
+
+
     });
 
 
