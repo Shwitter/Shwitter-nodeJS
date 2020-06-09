6a45775a7f0c225e1ab8dd7993a4dd5ce9385771
glontianano <glonti1899@gmail.com>
2020-06-10 02:06:22 +0400

    Manage file structure.

--- app.js
+++ app.js
@@ -10,12 +10,9 @@ const {log, ExpressAPILogMiddleware} = require('@rama41222/node-logger');
 const app = express();
 const userRouter = require("./src/routes/user");
 
-// const chatRouter = require("./src/routes/chat");
 const http = require('http').createServer(app);
 const io = require('socket.io')(http, { origins: '*:*'});
-const userModel = require("./src/models/userModel");
-const chatModel = require("./src/models/chatModel")
-let jwtDecode = require('jwt-decode');
+require('./src/lib/socket')(io);
 
 const config = {
     name: 'shwitter',
@@ -40,113 +37,6 @@ mongoose.set('useCreateIndex', true);
 const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
-
-let users = {};
-
-
-io.on('connection', function (socket) {
-    //Create Users array with all connected user.
-    socket.on('new-user', function (data) {
-        let jwt = data.jwt;
-        let decodedJwt = jwtDecode(jwt);
-        let id = decodedJwt.user.id;
-        userModel.findById(id).then(function (data) {
-            socket.username = data.username;
-            // username should be equal to user socket
-            // to defined receiver in private messaging.
-            users[socket.username] = socket;
-        });
-
-    })
-
-    // Send old messages to client.
-    socket.on('old-messages', function (data) {
-        let receiver = data.receiver;
-        let jwt = data.sender;
-        let decodedJwt = jwtDecode(jwt);
-        let id = decodedJwt.user.id;
-        userModel.findById(id).then(function (data) {
-            let sender = data.username;
-            let roomName = sender.concat('', receiver);
-            let roomName2 = receiver.concat('', sender);
-
-            chatModel.find({roomName: roomName}).then(function (doc) {
-                if (doc.length > 0) {
-                    users[sender].emit('old-messages', doc);
-                }
-                else {
-                    chatModel.find({roomName: roomName2}).then(function (doc) {
-                        if (doc.length > 0) {
-                            users[sender].emit('old-messages', doc);
-                        }
-                    })
-                }
-            })
-
-        });
-
-    })
-
-    // Send message in real time if user is online.
-    // And save in database.
-    socket.on('send-message', function (msg) {
-        let message = msg.message;
-        let receiver = msg.receiver;
-        let jwt = msg.sender;
-        let decodedJwt = jwtDecode(jwt);
-        let id = decodedJwt.user.id;
-        userModel.findById(id).then(function (user) {
-            let sender = user.username;
-            let roomName = sender.concat('', receiver);
-            let roomName2 = receiver.concat('', sender);
-
-            //Check if roomName already exists.
-            chatModel.find({roomName: roomName}).then(function (doc) {
-                if (doc.length > 0) {
-                    sendMessage(roomName);
-                }
-                else {
-                    chatModel.find({roomName: roomName2}).then(function (doc) {
-                        if (doc.length > 0) {
-                            sendMessage(roomName2);
-                        }
-                        // else create new room
-                        else {
-                            sendMessage(roomName);
-                        }
-                    })
-                }
-            })
-
-            function sendMessage(roomName){
-                let newMessage = new chatModel({
-                    roomName: roomName,
-                    sender: sender,
-                    message: message,
-                    receiver: receiver,
-                });
-                newMessage.save(function (err) {
-                    if (err) throw err;
-                    //If receiver is connected (is online), send message.
-                    if (receiver in users) {
-                        users[receiver].emit('new-message', {message: message, sender: sender});
-                    }
-                })
-            }
-
-        });
-
-
-    });
-
-
-    socket.on("disconnect", function (data) {
-        if (!socket.username) return;
-        delete users[socket.username];
-
-        console.log("Disconnected")
-    })
-});
 
 //Routers.
 app.use('/user', userRouter);
